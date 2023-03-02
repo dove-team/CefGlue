ba56dfda9de4145705809365fcaead21bc8074bd
Ding <michael19950415@outlook.com>
2023-03-02 21:54:10 +0800

    upload

--- CefGlue.GtkDemo/Program.cs
+++ CefGlue.GtkDemo/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace CefGlue.GtkDemo
+﻿using CefGlue.Gtk.Core;
+using CefGlue.Gtk.Interop;
+using CefGlue.Gtk;
+using Gtk;
+
+namespace CefGlue.GtkDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var runtime = new Runtime();
+            runtime.Initialize();
+            Application.Init();
+            using var window = new Window("ChromiumGTK Demo")
+            {
+                WidthRequest = 1200,
+                HeightRequest = 800
+            };
+            window.Destroyed += (sender, args) => runtime.QuitMessageLoop();
+            InteropLinux.SetDefaultWindowVisual(window.Handle);
+            using var webView = new WebView();
+            webView.LoadUrl("https://dotnet.microsoft.com/");
+            window.Add(webView);
+            window.ShowAll();
+            runtime.RunMessageLoop();
+            runtime.Shutdown();
         }
     }
 }
